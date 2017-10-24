c9c652a6082e9dc3c0461ceb22bb691f7ef775a4
Robert Krieg <hello@robertkrieg.ch>
2017-10-24 09:29:59 +0200

    add homestead configs

---
+++ configs/homestead/after.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# If you would like to do some extra provisioning you may
+# add any commands you wish to this file and they will
+# be run after the Homestead machine is provisioned.
+
+
+
+# define your sites config directory located on your host machine
+SITES=/home/vagrant/nginx-configs/*
+
+# copy every site file from /home/vagrant/setup/sites/ to your Nginx sites-available folder
+yes | sudo cp -rf $SITES /etc/nginx/sites-available
+
+# enable each site by creating a symbolic link to each file
+for p in $SITES
+do
+    FILE=$(basename $p)
+    sudo ln -s -f /etc/nginx/sites-available/$FILE /etc/nginx/sites-enabled/
+done
+
+# restart the nginx service
+sudo service nginx restart
+
+echo "** Copied custom nginx config files! **"
