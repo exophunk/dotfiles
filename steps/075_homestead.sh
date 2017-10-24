b13c20e8905272a877fe9da238c7df94fe22a4c3
Robert Krieg <hello@robertkrieg.ch>
2017-10-24 09:44:10 +0200

    Step to install homestead repo

---
+++ steps/075_homestead.sh
@@ -0,0 +1,8 @@
+###############################################################################
+# Homestead                                                                   #
+###############################################################################
+
+
+e_header "Install Homestead"
+
+git clone https://github.com/laravel/homestead.git ~/workspace/Homestead
