afaea8360addab11b24fcbeaeef9fd0c20f5485c
Robert Krieg <hello@robertkrieg.ch>
2017-10-30 15:31:00 +0100

    add zsh-nvm plugin

--- steps/095_oh-my-zsh.sh
+++ steps/095_oh-my-zsh.sh
@@ -10,3 +10,6 @@ e_header "Oh my Zsh Install"
 e_arrow "Installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+
+# Add zsh-nvm plugin
+git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
