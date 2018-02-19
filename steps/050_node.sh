6cffdecfdfa1491913feac3ee69ded2e2e2b5e3c
Robert Krieg <hello@robertkrieg.ch>
2018-02-19 14:56:09 +0100

    install more nvm versions and reinstall global npm packages for them

--- steps/050_node.sh
+++ steps/050_node.sh
@@ -3,13 +3,27 @@
 ###############################################################################
 
 
+e_header "Installing different node versions"
+
+nvm install v6.3.1
+nvm install v6.4.0
+nvm install v7.2.1
+nvm install v7.3.0
+nvm use default
+
+
+
+
 e_header "Installing Global Node Packages"
 
 packages=(
 	gulp
 	bower
 	eslint
+	eslint-plugin-vue
+	babel-eslint
 	electron
+	npm-check-updates
 )
 
 npm install -g "${packages[@]}"
@@ -21,3 +35,20 @@ e_header "Set NPM Configs"
 npm config set save true
 
 
+
+
+e_header "Copy global packages to other versions"
+
+nvm use v6.3.1
+nvm reinstall-packages default
+
+nvm use v6.4.0
+nvm reinstall-packages default
+
+nvm use v7.2.1
+nvm reinstall-packages default
+
+nvm use v7.3.0
+nvm reinstall-packages default
+
+nvm use default
