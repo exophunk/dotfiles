2499b36085e60e0f55325b6194f9c5503a362ca0
Robert Krieg <hello@robertkrieg.ch>
2018-02-19 14:55:47 +0100

    add new MAS apps

--- steps/040_appstore.sh
+++ steps/040_appstore.sh
@@ -12,6 +12,7 @@ mas install 425424353 # The Unarchiver
 mas install 1091189122 # Bear
 mas install 409201541 # Pages
 mas install 409183694 # Keynotes
+mas install 409203825 # Numbers
 mas install 668208984 # Giphy Gifmaker
 
 
