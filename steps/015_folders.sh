9e90f1579214a50438a606a3822498af43e0f308
Robert Krieg <hello@robertkrieg.ch>
2018-02-20 15:02:41 +0100

    add step to create folders

---
+++ steps/015_folders.sh
@@ -0,0 +1,21 @@
+###############################################################################
+# Fodlers                                                                     #
+###############################################################################
+
+
+# Add folders
+e_header "Add folders"
+
+mkdir '~/workspace'
+mkdir '~/workspace/y7k'
+mkdir '~/workspace/y7k/other'
+mkdir '~/workspace/y7k/plates'
+mkdir '~/workspace/y7k/projects'
+mkdir '~/workspace/y7k/prototypes'
+mkdir '~/workspace/testing'
+mkdir '~/workspace/personal'
+
+
+e_success "Done!"
+
+
