9de1123517cd680c368aacff878819cc34d80dc5
Robert Krieg <hello@robertkrieg.ch>
2018-02-20 15:02:07 +0100

    install oh my zsh separately

--- init.sh
+++ init.sh
@@ -1,9 +1,11 @@
 ###############################################################################
-# Oh my ZSH                                                                   #
+# Init                                                                        #
 ###############################################################################
 
+e_header "INIT"
 
-e_header "Oh my Zsh Plugins"
+
+e_header "Oh my Zsh Install"
 
 # Add zsh-nvm plugin
 git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
