64653794b9a235a4f65be805739a81d814a089ee
Robert Krieg <hello@robertkrieg.ch>
2018-02-22 09:46:04 +0100

    add some more fonts

--- steps/035_brew-cask-fonts.sh
+++ steps/035_brew-cask-fonts.sh
@@ -12,18 +12,28 @@ brew tap caskroom/fonts
 e_arrow "Installing cask fonts"
 
 fonts=(
+	font-arial
+	font-arvo
+	font-chivo
 	font-clear-sans
+	font-comfortaa
+	font-comic-neue
+	font-crimson-text
+	font-droid-sans
+	font-fenix
 	font-fira-code
 	font-fira-sans
 	font-inconsolata
+	font-jaapoki
 	font-lato
+	font-lora
 	font-open-sans
 	font-open-sans-condensed
+	font-raleway
 	font-roboto
 	font-source-code-pro
 	font-source-sans-pro
 	font-source-serif-pro
-	font-droid-sans
 )
 
 for font in "${fonts[@]}"; do
