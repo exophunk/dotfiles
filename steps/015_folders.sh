ff3cc05c8188467b0757e2f71277de8be63ced5d
Robert Krieg <hello@robertkrieg.ch>
2018-02-22 16:50:31 +0100

    add apps and fix folder steps

--- steps/015_folders.sh
+++ steps/015_folders.sh
@@ -6,14 +6,14 @@
 # Add folders
 e_header "Add folders"
 
-mkdir '~/workspace'
-mkdir '~/workspace/y7k'
-mkdir '~/workspace/y7k/other'
-mkdir '~/workspace/y7k/plates'
-mkdir '~/workspace/y7k/projects'
-mkdir '~/workspace/y7k/prototypes'
-mkdir '~/workspace/testing'
-mkdir '~/workspace/personal'
+mkdir '${HOME}/workspace'
+mkdir '${HOME}/workspace/y7k'
+mkdir '${HOME}/workspace/y7k/other'
+mkdir '${HOME}/workspace/y7k/plates'
+mkdir '${HOME}/workspace/y7k/projects'
+mkdir '${HOME}/workspace/y7k/prototypes'
+mkdir '${HOME}/workspace/testing'
+mkdir '${HOME}/workspace/personal'
 
 
 e_success "Done!"
