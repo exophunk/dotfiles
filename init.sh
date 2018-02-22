220c3aa35f5df23d73b054cb6f76e1dcc8b2a210
Robert Krieg <hello@robertkrieg.ch>
2018-02-22 10:00:30 +0100

    fix init file

--- init.sh
+++ init.sh
@@ -2,10 +2,16 @@
 # Init                                                                        #
 ###############################################################################
 
+# include utils
+for file in utils/*.sh
+	do . $file
+done
+
+
 e_header "INIT"
 
 
-e_header "Oh my Zsh Install"
+e_header "ZSH and Oh my Zsh Install"
 
-# Add zsh-nvm plugin
+# Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
