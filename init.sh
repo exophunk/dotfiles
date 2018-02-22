4e85b621db648f4eabc4e3c2278fbd74171e1848
Robert Krieg <hello@robertkrieg.ch>
2018-02-22 09:45:58 +0100

    fix init.sh for ohmyzh installation

--- init.sh
+++ init.sh
@@ -8,4 +8,4 @@ e_header "INIT"
 e_header "Oh my Zsh Install"
 
 # Add zsh-nvm plugin
-git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
